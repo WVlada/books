902ad8a12804b28619ddacaa698b2591e5ed6f91
WVlada <vladimir.bukvic@live.com>
2020-01-15 16:05:49 +0100

    seed delete linked stuff on logout

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const Company = require("./company")
+const Company = require("./company");
 const Nalog = require("./nalog");
 const Stav = require("./stav");
 const Konto = require("./konto");
@@ -74,36 +74,44 @@ userSchema.methods.setActiveCompany = function(company) {
 };
 userSchema.methods.createMoreCompanies = async function(company) {
   // ne moze da se nalazi u seed_ostali nalozi jer u trenutku kad pozivam jos nije exportovan Company Model
-const company2 = await Company.create({
-  year: [company.year[0], company.year[0] + 1, company.year[0] - 1],
-  vrste_naloga: ["R", "N", "I", "Z"],
-  name: "Software Inc.",
-  mb: "12332112",
-  pib: "123321123",
-  user: this
-})
-const company3 = await Company.create({
-  year: [company.year[0], company.year[0] + 1, company.year[0] - 1],
-  vrste_naloga: ["R", "N", "I", "Z"],
-  name: "Software Commerce Inc.",
-  mb: "23123121",
-  pib: "789987789",
-  user: this
-})
-// company
-}
+  const company2 = await Company.create({
+    year: [company.year[0], company.year[0] + 1, company.year[0] - 1],
+    vrste_naloga: ["R", "N", "I", "Z"],
+    name: "Software Inc.",
+    mb: "12332112",
+    pib: "123321123",
+    user: this
+  });
+  const company3 = await Company.create({
+    year: [company.year[0], company.year[0] + 1, company.year[0] - 1],
+    vrste_naloga: ["R", "N", "I", "Z"],
+    name: "Software Commerce Inc.",
+    mb: "23123121",
+    pib: "789987789",
+    user: this
+  });
+  // company
+};
 
-userSchema.methods.deleteAllConnectedRecords = async function(){
-  let companies = []
-  await Company.find({user: this}).then(resArray=>{
-    if (resArray.length > 0 ){
-      for(let i = 0;i <= resArray.length - 1; i++ ){
-        companies.push(resArray[i]._id)
+userSchema.methods.deleteAllConnectedRecords = async function() {
+  let companies = [];
+  await Company.find({ user: this }).then(resArray => {
+    if (resArray.length > 0) {
+      for (let i = 0; i <= resArray.length - 1; i++) {
+        companies.push(resArray[i]._id);
       }
     }
-  })
+  });
   // i have all company _ids
-
-}
+  for (let j = 0; j <= companies.length - 1; j++) {
+    await Nalog.deleteMany({ company: companies[j] });
+    await Stav.deleteMany({ company: companies[j] });
+    await Komitent.deleteMany({ company: companies[j] });
+    await Konto.deleteMany({ company: companies[j] });
+    await Komitenttype.deleteMany({ company: companies[j] });
+    await Company.deleteMany({ _id: companies[j] });
+  }
+  console.log("All LinkedIn stuff deleted.");
+};
 
 module.exports = mongoose.model("User", userSchema);
