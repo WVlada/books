be62ab82835b638e8b3b846cf6b3f473a53de188
WVlada <vladimir.bukvic@live.com>
2020-02-18 21:52:06 +0100

    komitent edit part 2

--- routes/komitent.js
+++ routes/komitent.js
@@ -10,6 +10,7 @@ const komitentController = require("../controllers/komitent");
 
 router.get("/komitenti", isAuth, komitentController.getKomitentIndex);
 router.get("/komitent", isAuth, komitentController.getKomitent);
+router.get("/komitent_new", isAuth, komitentController.getNewKomitent);
 router.get("/komitent_edit", isAuth, komitentController.getEditKomitent);
 
 module.exports = router;
